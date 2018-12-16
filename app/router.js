a15e812c5ad8aa0fb22a3979f005e844e69b6d05
Daniel Kanas <dkanas95@gmail.com>
2018-12-16 02:59:17 +0100

    router file

---
+++ app/router.js
@@ -0,0 +1,14 @@
+const moviesController = require('./controllers/movies')
+const commentsController = require('./controllers/comments')
+
+const router = async fastify => {
+  // movies routes
+  fastify.get('/movies', moviesController.getAll)
+  fastify.post('/movies', moviesController.create)
+
+  // comments routes
+  fastify.get('/comments', commentsController.getAll)
+  fastify.post('/comments', commentsController.create)
+}
+
+module.exports = router
