373b8ca46ea8cdbffe4752248f790e97414bc413
Daniel Kanas <dkanas95@gmail.com>
2018-12-17 21:35:24 +0100

    comments model schema update

--- app/models/comment.js
+++ app/models/comment.js
@@ -4,16 +4,25 @@ const mongoose = require('mongoose')
 // comment lengths - it's also the limit supposedly used by Facebook:
 // https://sproutsocial.com/insights/social-media-character-counter/#facebook
 const COMMENT_MAX_LENGTH = 8000
+const COMMENT_MIN_LENGTH = 5
 
-const commentSchema = new mongoose.Schema({
-  author: String,
-  content: {
+const CommentSchema = new mongoose.Schema({
+  author: {
     type: String,
-    maxlength: COMMENT_MAX_LENGTH
+    required: true
   },
-  movie: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }
+  content: {
+    required: true,
+    type: String,
+    maxlength: COMMENT_MAX_LENGTH,
+    minLength: COMMENT_MIN_LENGTH
+  },
+  movie: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
 })
 
-const Comment = mongoose.model('Comment', commentSchema)
+const Comment = mongoose.model('Comment', CommentSchema)
 
 module.exports = Comment
