a025ec9fbe2bab30f222c556e98d7d66ff523fc5
Daniel Kanas <dkanas95@gmail.com>
2018-12-18 00:45:24 +0100

    comments controller

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -1,8 +1,45 @@
+const { ObjectId } = require('mongoose').Types
 const Comment = require('../models/comment')
+const Movie = require('../models/movie')
+const { parseAndValidateLimit } = require('../helpers/common')
+const buildError = require('../helpers/error')
 
-const commentsController = model => ({
-  getAll: (req, rep) => model.find().then(data => rep.send(data)),
-  create: (req, rep) => rep.send(req.body)
+const PAGE_LIMIT = 10
+function buildQuery(model, params) {
+  const { limit, page, movie } = params
+
+  const validLimit = parseAndValidateLimit(limit, PAGE_LIMIT)
+  const parsedPage = Number(page)
+  const query = model.find()
+  if (movie) query.where('movie').equals(movie)
+  if (parsedPage) query.skip(validLimit * parsedPage)
+  query.limit(validLimit)
+
+  return query.lean().exec()
+}
+const commentsController = (commentModel, movieModel) => ({
+  get: async req => {
+    return await buildQuery(commentModel, req.query)
+  },
+  create: async (req, rep) => {
+    const { author, content, movie } = req.body
+
+    if (!author) throw buildError(400, 'Author name missing!')
+    if (!content) throw buildError(400, 'Content missing!')
+    if (!movie) throw buildError(400, 'Movie id missing!')
+    if (!ObjectId.isValid(movie)) throw buildError(400, 'Invalid movie id!')
+    const assignedMovie = await movieModel.findById(movie)
+    if (!assignedMovie) throw buildError(404, 'Assigned movie does not exist!')
+
+    try {
+      rep.status(201)
+      return await commentModel.create(req.body)
+    } catch (err) {
+      if (err.name === 'ValidationError') {
+        throw buildError(400, 'Validation error!')
+      }
+    }
+  }
 })
 
-module.exports = commentsController(Comment)
+module.exports = commentsController(Comment, Movie)
