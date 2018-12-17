4bf5c94e1f89f81db8d62359c17f3dd5f2252c2b
Daniel Kanas <dkanas95@gmail.com>
2018-12-17 01:30:44 +0100

    seed now as exported func

--- app/db/seed.js
+++ app/db/seed.js
@@ -4,12 +4,12 @@ const Comment = require('../models/comment')
 const movies = require('./seedData/movies.json')
 const comments = require('./seedData/comments.json')
 
-connectToDB().then(async () => {
-  console.log('Clearing and seeding DB...')
-  await Movie.deleteMany()
-  await Comment.deleteMany()
-  await Movie.create(movies)
-  await Comment.create(comments)
-  console.log('Done!')
-  process.exit()
-})
+module.exports = () =>
+  connectToDB().then(async () => {
+    console.log('Clearing and seeding DB...')
+    await Movie.deleteMany()
+    await Comment.deleteMany()
+    await Movie.create(movies)
+    await Comment.create(comments)
+    console.log('Done!')
+  })
