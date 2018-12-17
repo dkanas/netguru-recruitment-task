473a4392a8a76273d8d6d72f8f12dec9313cc2ab
Daniel Kanas <dkanas95@gmail.com>
2018-12-17 21:34:16 +0100

    movies GET tests

--- test/controllers/movies.test.js
+++ test/controllers/movies.test.js
@@ -1,8 +1,28 @@
-const expect = require('chai').expect
+const chai = require('chai')
 const mongoose = require('mongoose')
 const server = require('../../app/server')
 const seedDb = require('../../app/db/seed')
 const moviesSeedData = require('../../app/db/seedData/movies.json')
+
+const { expect } = chai
+
+const checkForParams = async (params, expectedSeedData) => {
+  const res = await server.inject({
+    method: 'GET',
+    url:
+      '/movies?' +
+      Object.entries(params)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('&')
+  })
+
+  const body = JSON.parse(res.body)
+  expect(body.length).to.equal(expectedSeedData.length)
+  body.forEach(movie => {
+    const movieInExpectedSeedData = expectedSeedData.find(m => m._id === movie._id)
+    expect(movie).to.eql(movieInExpectedSeedData)
+  })
+}
 
 describe('Movies controller', () => {
   // hooks for setup and teardown
@@ -23,17 +43,21 @@ describe('Movies controller', () => {
       })
 
       const movies = JSON.parse(res.body)
-      expect(movies).to.have.lengthOf.at.most(10)
+      expect(movies).to.have.lengthOf(10)
     })
 
     it('should limit results based on query', async () => {
-      const res = await server.inject({
-        method: 'GET',
-        url: '/movies?limit=5'
-      })
+      try {
+        const res = await server.inject({
+          method: 'GET',
+          url: '/movies?limit=5'
+        })
 
-      const movies = JSON.parse(res.body)
-      expect(movies).to.have.lengthOf.at.most(5)
+        const movies = JSON.parse(res.body)
+        expect(movies).to.have.lengthOf(5)
+      } catch (err) {
+        console.error(err)
+      }
     })
 
     it('should not exceed default limit', async () => {
@@ -43,20 +67,93 @@ describe('Movies controller', () => {
       })
 
       const movies = JSON.parse(res.body)
-      expect(movies).to.have.lengthOf.at.most(10)
+      expect(movies).to.have.lengthOf(10)
     })
 
     it('should return movie based on imdb id', async () => {
       const imdbId = 'tt0068646'
-      const res = await server.inject({
-        method: 'GET',
-        url: '/movies?imdbId=' + imdbId
-      })
+      const expectedSeedData = moviesSeedData.filter(m => m.imdbId === imdbId)
+      await checkForParams({ imdbId }, expectedSeedData)
+    })
 
-      const movies = JSON.parse(res.body)
-      const seedDataMovie = moviesSeedData.find(m => m.imdbId === imdbId)
-      expect(movies).to.have.lengthOf(1)
-      expect(movie).to.eql(seedDataMovie)
+    it('should return movie based on id', async () => {
+      const id = '5c15afd96197100a1201cc59'
+      const expectedSeedData = moviesSeedData.filter(m => m._id === id)
+      await checkForParams({ id }, expectedSeedData)
+    })
+
+    it('should return movie based on title', async () => {
+      const title = 'The Godfather'
+      const expectedSeedData = moviesSeedData.filter(m => m.title === title)
+      await checkForParams({ title }, expectedSeedData)
+    })
+
+    it('should return movies based on search param', async () => {
+      const title = 'the godfather'
+      const expectedSeedData = moviesSeedData.filter(m => {
+        const rgx = new RegExp(title, 'i')
+        rgx.test(m.title)
+      })
+      await checkForParams({ title }, expectedSeedData)
+    })
+
+    it('should return movies released in given years range', async () => {
+      const minYear = 1990
+      const maxYear = 1995
+      const expectedSeedData = moviesSeedData
+        .filter(m => m.year >= minYear && m.year <= maxYear)
+        .slice(0, 10)
+      await checkForParams({ minYear, maxYear }, expectedSeedData)
+    })
+
+    it('should return movies with given runtime', async () => {
+      const minRuntime = 130
+      const maxRuntime = 135
+      const expectedSeedData = moviesSeedData
+        .filter(m => m.runtime >= minRuntime && m.runtime <= maxRuntime)
+        .slice(0, 10)
+      await checkForParams({ minRuntime, maxRuntime }, expectedSeedData)
+    })
+
+    it('should return movies with given genre', async () => {
+      const genre = 'Drama'
+      const expectedSeedData = moviesSeedData.filter(m => m.genre.includes(genre)).slice(0, 10)
+      await checkForParams({ genre }, expectedSeedData)
+    })
+
+    it('should return movies with given actor', async () => {
+      const actor = 'Marlon Brando'
+      const expectedSeedData = moviesSeedData.filter(m => m.actors.includes(actor)).slice(0, 10)
+      await checkForParams({ actor }, expectedSeedData)
+    })
+
+    it('should return movies with given director', async () => {
+      const director = 'Francis Ford Coppola'
+      const expectedSeedData = moviesSeedData
+        .filter(m => m.director.includes(director))
+        .slice(0, 10)
+      await checkForParams({ director }, expectedSeedData)
+    })
+
+    it('should return movies with given language', async () => {
+      const language = 'English'
+      const expectedSeedData = moviesSeedData
+        .filter(m => m.language.includes(language))
+        .slice(0, 10)
+      await checkForParams({ language }, expectedSeedData)
+    })
+
+    it('should return movies with given country', async () => {
+      const country = 'USA'
+      const expectedSeedData = moviesSeedData.filter(m => m.country.includes(country)).slice(0, 10)
+      await checkForParams({ country }, expectedSeedData)
+    })
+
+    it('should get second page', async () => {
+      // count from 0
+      const page = 1
+      const expectedSeedData = moviesSeedData.slice(10, 20)
+      await checkForParams({ page }, expectedSeedData)
     })
   })
 })
